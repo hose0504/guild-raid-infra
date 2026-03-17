f002c7b963b9c873b5080003acf7280e60663f44
unknown <hose0504@naver.com>
2026-03-17 20:30:31 +0900

    10th commit

--- outputs.tf
+++ outputs.tf
@@ -41,11 +41,3 @@ output "record_fqdn" {
 output "certificate_arn" {
   value = module.acm.certificate_arn
 }
-
-output "record_fqdn" {
-  value = module.route53_alias.record_fqdn
-}
-
-output "alb_dns_name" {
-  value = module.alb.alb_dns_name
-}
